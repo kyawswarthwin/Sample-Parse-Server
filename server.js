5fb6300ae0d52e2e28e3b3bb335c9c8e115ace53
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2018-01-23 21:33:48 +0630

    add cloud code

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ const app = express();
 
 const port = process.env.PORT || 1337;
 const api = new ParseServer({
-  databaseURI: process.env.MONGODB_URI || "mongodb://localhost:27017/dev",
   appId: process.env.APP_ID || "myAppId",
-  masterKey: process.env.MASTER_KEY || "myMasterKey"
+  masterKey: process.env.MASTER_KEY || "myMasterKey",
+  databaseURI: process.env.MONGODB_URI || "mongodb://localhost:27017/dev",
+  port: port,
+  cloud: path.join(__dirname, "cloud/main.js")
 });
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/parse", api);
 
-app.listen(port, () => {
+const httpServer = require("http").createServer(app);
+httpServer.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
+ParseServer.createLiveQueryServer(httpServer);
