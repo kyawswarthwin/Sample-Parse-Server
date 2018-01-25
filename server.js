b0e4ae909c503998b48c1e74c245f26bfccdb365
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2018-01-25 20:26:21 +0630

    change tidy code

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-"use strict";
-const express = require("express");
-const ParseServer = require("parse-server").ParseServer;
+'use strict';
+const express = require('express');
+const ParseServer = require('parse-server').ParseServer;
 
 const app = express();
 
 const port = process.env.PORT || 1337;
 const api = new ParseServer({
-  databaseURI: process.env.MONGODB_URI || "mongodb://localhost:27017/dev",
-  appId: process.env.APP_ID || "myAppId",
-  masterKey: process.env.MASTER_KEY || "myMasterKey"
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || 'myMasterKey',
+  databaseURI: process.env.MONGODB_URI || 'mongodb://localhost:27017/dev',
+  port: port
 });
 
-app.use("/parse", api);
+app.use('/parse', api);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
